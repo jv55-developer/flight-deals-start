eaafba56209f60f59df001304c4321278b8f6053
Jason Vandeyar <jasonvandeyar55@gmail.com>
2023-07-17 05:19:50 +0200

    adding loop for getting prices from kiwi api

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import requests
-# import os
-#
-# TEQUILA_API = os.environ["TEQUILA_API"]
-# TEQUILA_ENDPOINT = os.environ["TEQUILA_ENDPOINT"]
-# SHEETY_GET_ENDPOINT = os.environ["SHEETY_GET_ENDPOINT"]
-#
-# url = TEQUILA_ENDPOINT
-# headers = {
-#     'apikey': TEQUILA_API
-# }
-#
-# sheety_get_endpoint = SHEETY_GET_ENDPOINT
-#
+import os
+
+TEQUILA_API = os.environ["TEQUILA_API"]
+TEQUILA_ENDPOINT = os.environ["TEQUILA_ENDPOINT"]
+SHEETY_GET_ENDPOINT = os.environ["SHEETY_GET_ENDPOINT"]
+TEQUILA_API_SEARCH = os.environ["TEQUILA_API_SEARCH"]
+
+url = TEQUILA_ENDPOINT
+url_2 = TEQUILA_API_SEARCH
+headers = {
+    'apikey': TEQUILA_API
+}
+
+sheety_get_endpoint = SHEETY_GET_ENDPOINT
+
+# Get IATA codes for Google sheet
 # sheety_get_response = requests.get(url=sheety_get_endpoint)
 #
 # sheety_get_data = sheety_get_response.json()['sheet1']
@@ -27,6 +30,8 @@ import requests
 #     iata_code = response.json()['locations'][0]['code']
 #
 #     print(iata_code)
+
+
 
 
 
